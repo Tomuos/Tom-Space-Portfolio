0472ae789b704c8db9e3e3aa1d341fb379cd8e55
Tom Burns <Tomulvlup@gmail.com>
2023-11-06 21:23:51 +0000

    style changes

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,8 +2,9 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image'
 
 
 const HeroContent = () => {
@@ -21,10 +22,42 @@ const HeroContent = () => {
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5  ' />
           <h1 className='Welcome-text text-[13px] ' >Frontend Developer</h1>  
         </motion.div>
+        <motion.div
+        variants={slideInFromLeft(0.5)}
+        className='flex flex-col gap-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+        >
+          Providing
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> The Best</span>
+          Project Experience
+        </motion.div>
+
+        <motion.p
+        variants={slideInFromLeft(0.8)}
+        className='text-lg text-gray-400 my-5 max-w-[600px] '>
+        Hi, I&apos;m Tom and I&apos;m a Frontend Developer. I have a passion for web design and love to create for web and mobile devices. I&apos;m currently working as a freelancer while searching for fulltime work.  
+        </motion.p>
+        <motion.a
+        variants={slideInFromLeft(1)}
+        className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] "
+        >
+          Learn More
+        </motion.a>
+
         </div>
 
+      <motion.div
+      variants={slideInFromRight(0.8)}
+      className="w-full h-full flex justify-center items-center"
+      >
+        <Image 
+        src="/mainiconsdark.svg"
+        alt="Work Icons"
+        height={650}
+        width={650}
+        />
+      </motion.div>
     </motion.div>
-  )
+  );
 }
 
 export default HeroContent
